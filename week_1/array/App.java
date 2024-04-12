88af7bd76031c78006588797a4734faea6745c11
Jay Arre P. Talosig <35078433+flexycode@users.noreply.github.com>
2024-04-12 11:07:13 +0000

    📓 Add array

---
+++ week_1/array/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) {
+
+        // Array of Strings
+        String[] myfavefastfood = "KuyaJay", "AlingTising", "BurgerKing", "BonChon",
+        "KennyRogers" };
+
+        // Array of Integers
+        int[] primeNumbers = { 2, 3, 5, 7 };
+
+        // Array of Characters
+        char[] myName = { 'J', 'A', 'Y', 'A', 'R', 'R', 'E' };
+    }
+}
