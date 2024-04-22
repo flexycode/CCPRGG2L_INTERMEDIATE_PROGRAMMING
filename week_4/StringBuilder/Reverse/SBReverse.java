ab6b1270f5d9131a37fd987cc57d0e86ea7c9757
Jay Arre P. Talosig <35078433+flexycode@users.noreply.github.com>
2024-04-22 17:18:40 +0000

    📓 Added Reverse

---
+++ week_4/StringBuilder/Reverse/SBReverse.java
@@ -0,0 +1,15 @@
+package Intermediate;
+
+public class SBReverse {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Programmer Ako");
+
+        // Reverse the characters in the StringBuilder
+        sb.reverse();
+
+        System.out.println(sb.toString());
+        
+    }
+    
+}
